950f7c1c794b9dc26cc305d4b0a106f02c3857dd
dongjunchen <dongjunchen@sensetime.com>
2023-09-23 13:09:50 +0800

    amend code

--- DataFetch.py
+++ DataFetch.py
@@ -12,22 +12,31 @@ class WindDataFetch:
     def __del__(self) -> None:
         w.stop()
     
+    '''
+    @return 
+    {
+        "SH_00213":DataFrame,
+        "SH_00213":DataFrame,
+    }
+    DataFrame format:
+                 OPEN  CLOSE   HIGH    LOW   PCT_CHG      TURN    datetime
+    2023-02-23  14.00  14.05  14.32  13.98  0.213980  0.424874  2023-02-23
+    2023-02-24  14.02  13.86  14.03  13.80 -1.352313  0.376175  2023-02-24
+    2023-02-27  13.75  13.69  13.88  13.68 -1.226551  0.320250  2023-02-27
+    2023-03-01  13.80  14.17  14.19  13.74  2.830189  0.630465  2023-03-01
+    '''
     def fetch_data(self, codes_set:set, from_date:datetime, to_date:datetime, fields:list) -> pd.DataFrame:
         if not w.isconnected():
             logging.error('wind not inited')
             return None
-        # 任取一只国债010107.SH六月份以来的净值历史行情数据
-        history_data=w.wsd("010107.SH",
-                   "sec_name,ytm_b,volume,duration,convexity,open,high,low,close,vwap", 
-                   "2018-06-01", "2018-06-11", "returnType=1;PriceAdj=CP", usedf=True) 
-        # returnType表示到期收益率计算方法，PriceAdj表示债券价格类型‘
-        print(history_data[1].head())
+        histroy_data = {}
         for code in codes_set:
             data_frame = w.wsd(codes = code,fields=fields,beginTime=from_date, endTime=to_date,usedfdt =True)[1]
-            print('dkjaskdjf')
+            data_frame['datetime'] = data_frame.index
+            histroy_data[code] = data_frame
+            print(data_frame)
             
-        print(data_frame[1].head())
-        return data_frame
+        return histroy_data 
     def fetch_codes(self,options):
         if not w.isconnected():
             logging.error('wind not inited')
